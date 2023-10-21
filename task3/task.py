2a2c287821552edb7d79d9817e87b56ee8dbcddf
dsrgva <77065650+dsrgva@users.noreply.github.com>
2023-10-21 19:53:42 +0300

    Update task.py

--- task3/task.py
+++ task3/task.py
@@ -1,1 +1,18 @@
+import csv
+from math import log2
 
+def task(csv_string):
+    csv_string = csv_string.split('\n')
+    reader = csv.reader(csv_string)
+    n = len(csv_string)
+    entropy_value = 0
+    for row in reader:
+        row_entropy = 0 
+        for value in map(int, row):
+            if value != 0:
+                element_entropy = value / (n - 1) * log2(value / (n - 1))
+                row_entropy += element_entropy
+        entropy_value -= row_entropy
+    return round(entropy_value, 1)
+
+print(task("1,0,4,0,0\n2,1,2,0,0\n2,1,0,1,1\n0,1,0,1,1\n0,1,0,2,1\n0,1,0,2,1"))
